3151068222cec339c2174f65586425626361ebbc
Ahruda <leogalbierearruda@gmail.com>
2023-09-14 20:10:47 -0300

    feature/01 - Projeto base

---
+++ src/main/java/com/audsat/carinsurance/carInsurance/CarInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.audsat.carinsurance.carInsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInsuranceApplication.class, args);
+	}
+
+}
