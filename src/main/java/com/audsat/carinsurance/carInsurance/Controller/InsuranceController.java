420af291555ec0f57c6db91d0f02ab7aa75d92cc
Ahruda <leogalbierearruda@gmail.com>
2023-09-15 08:35:16 -0300

    Cria pacote de controller

---
+++ src/main/java/com/audsat/carinsurance/carInsurance/Controller/InsuranceController.java
@@ -0,0 +1,12 @@
+package com.audsat.carinsurance.carInsurance.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("insurance/budget")
+public class InsuranceController {
+
+}
