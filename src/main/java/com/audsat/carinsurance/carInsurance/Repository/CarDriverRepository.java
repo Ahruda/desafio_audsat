090cc4cff70ec37ffa9cc990196f4d83944352d1
Ahruda <leogalbierearruda@gmail.com>
2023-09-17 09:49:02 -0300

    Adiciona consultas para as regras

--- src/main/java/com/audsat/carinsurance/carInsurance/Repository/CarDriverRepository.java
+++ src/main/java/com/audsat/carinsurance/carInsurance/Repository/CarDriverRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarDriverRepository extends JpaRepository<CarDriverEntity, Long> {
 
+    CarDriverEntity findByCarIdAndIsMainDriver(Long carId, Boolean aTrue);
 }
---
+++ src/main/java/com/audsat/carinsurance/carInsurance/Repository/ClaimRepository.java
@@ -0,0 +1,15 @@
+package com.audsat.carinsurance.carInsurance.Repository;
+
+import com.audsat.carinsurance.carInsurance.Entity.CarDriverEntity;
+import com.audsat.carinsurance.carInsurance.Entity.ClaimEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClaimRepository extends JpaRepository<ClaimEntity, Long> {
+
+    List<ClaimEntity> findByDriverId(Long driverId);
+
+    List<ClaimEntity> findByCarId(Long carId);
+
+}
